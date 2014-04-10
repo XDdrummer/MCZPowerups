2e989cdf8073968b24783e0084ead1bdcc532b64
XDdrummer <xddrummeronmc@gmail.com>
2014-04-10 17:11:42 -0400

    Updated the command. It didn't exist.

--- src/plugin/XDdrummer/PowerUps/PowerupsListCommand.java
+++ src/plugin/XDdrummer/PowerUps/PowerupsListCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.ChatColor;
 
 public class PowerupsListCommand implements CommandExecutor {
 	Logger log = Bukkit.getLogger();
@@ -21,6 +22,15 @@ public class PowerupsListCommand implements CommandExecutor {
 		if(s instanceof Player) {
 			Player p = (Player) s;
 			if(p.hasPermission("powerups.admin")||p.isOp()) {
+				p.sendMessage(ChatColor.GREEN + "[MCZPowerups] Admin's List of MC-Z Powerups!" + "\n"
+				+ ChatColor.RED + "Note- in order to be effected by powerups, must have perm 'powerups.active'" + "\n"
+				+ ChatColor.BLUE + "Diamond blocks provide 20 seconds of Jump + Regeneration." + "\n"
+				+ ChatColor.GREEN + "Emerald blocks provide 20 seconds of Strentgh + Night Vision." + "\n"
+				+ ChatColor.YELLOW + "Gold blocks provide 20 seconds of Speed + Fire Resistance." + "\n"
+				+ ChatColor.AQUA + "Beacons provide 60 seconds of Damage Resistance, but 30 seconds of Confusion." + "\n"
+				+ ChatColor.BLUE + "Water is poisonous! It gives you 10 seconds of Confusion + Hunger." + "\n"
+				+ ChatColor.RED + "Lava weakens you! It gives you 5 seconds of weakness." + "\n"
+				+ ChatColor.AQUA + "Warning! You can not breathe under water!");
 
 			}
 		}else {
